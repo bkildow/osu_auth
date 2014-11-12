69dbbc7b5f4dbdba01ea6c1060810fcbe1614291
Brian Kildow <bkildow@gmail.com>
2014-11-12 12:19:33 -0500

    Fixed indents in initializer

--- lib/generators/osu_auth/install/install_generator.rb
+++ lib/generators/osu_auth/install/install_generator.rb
@@ -3,13 +3,13 @@ module OsuAuth
     source_root File.expand_path('../templates', __FILE__)
 
     def install
-      run 'bundle install'
+      # run 'bundle install'
       route "mount OsuAuth::Engine => '/admin'"
       rake 'OsuAuth:install:migrations'
       initializer 'osu_auth.rb' do
         "OsuAuth::Permission.config do
-        # Return permissions hash here e.g. {perm_name: 'description'}
-      end"
+  # Return permissions hash here e.g. {perm_name: 'description'}
+end"
       end
     end
   end
