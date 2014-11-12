836176ae10c890f3a8d7ee5ad26b9858a23316e8
Brian Kildow <bkildow@gmail.com>
2014-11-12 10:44:11 -0500

    Added permission configuration to model

--- app/models/osu_auth/permission.rb
+++ app/models/osu_auth/permission.rb
@@ -1,4 +1,14 @@
 module OsuAuth
   class Permission < ActiveRecord::Base
+
+    def self.config
+      perms = {edit_user: 'can edit users'}
+      config = block_given? ? yield(perms) : {}
+      @permissions = perms.merge(config)
+    end
+
+    def self.permissions
+      @permissions
+    end
   end
 end
--- test/models/osu_auth/permission_test.rb
+++ test/models/osu_auth/permission_test.rb
@@ -2,8 +2,13 @@ require 'test_helper'
 
 module OsuAuth
   class PermissionTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
+
+    test 'permissions should be merged' do
+      OsuAuth::Permission.config do
+        {perm1: 'permission one'}
+      end
+
+      assert_equal OsuAuth::Permission.permissions, {perm1: 'permission one', edit_user: 'can edit users'}
+    end
   end
 end
