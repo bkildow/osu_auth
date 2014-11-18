91395b058c59871f9c4a33637ed0a6451870ef2c
Brian Kildow <bkildow@gmail.com>
2014-11-18 10:56:20 -0500

    Renamed machine_name to permission

--- db/migrate/20141118155344_create_osu_auth_grants.rb
+++ db/migrate/20141118155344_create_osu_auth_grants.rb
@@ -2,7 +2,7 @@ class CreateOsuAuthGrants < ActiveRecord::Migration
   def change
     create_table :osu_auth_grants do |t|
       t.integer :role_id
-      t.string :machine_name
+      t.string :permission
 
       t.timestamps
     end
