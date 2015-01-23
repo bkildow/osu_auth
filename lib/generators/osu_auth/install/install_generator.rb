55603060f437dd080c49d2ae08bec675089fbf71
Brian Kildow <bkildow@gmail.com>
2015-01-23 10:32:28 -0500

    Refactoring installer

--- lib/generators/osu_auth/install/install_generator.rb
+++ lib/generators/osu_auth/install/install_generator.rb
@@ -6,16 +6,9 @@ module OsuAuth
       # run 'bundle install'
       route "mount OsuAuth::Engine => '/admin'"
       rake 'osu_auth:install:migrations'
-      initializer 'osu_auth.rb' do
-        "OsuAuth::Permission.config do
-  # Return permissions array here. Example:
-  # [
-  #   {name: :delete_users, description: 'can delete users'},
-  #   {name: :edit_resource, description: 'can edit resources'}
-  # ]
-end
-"
-      end
+
+      copy_file 'osu_auth.rb', "config/initializers/#{file_name}.rb"
+
     end
   end
 end
