d73bb2ce402cd5c473b5d7be5d0371b231c4e7ef
Brian Kildow <bkildow@gmail.com>
2015-01-26 08:35:28 -0500

    Simplified omniauth config

--- lib/generators/osu_auth/install/templates/omniauth.rb
+++ lib/generators/osu_auth/install/templates/omniauth.rb
@@ -1,12 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-
-  shib_options = {
-      shib_session_id_field: 'Shib-Session-ID',
-      shib_application_id_field: 'Shib-Application-ID',
-      debug: false
-  }
-
-  provider :shibboleth, shib_options
-
+  provider :shibboleth, { debug: false }
 end
