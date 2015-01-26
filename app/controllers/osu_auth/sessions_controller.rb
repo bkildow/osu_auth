cfdf56e0e88c25a7a15a8aff262012fe9a5f862c
Brian Kildow <bkildow@gmail.com>
2015-01-26 11:45:12 -0500

    Skip crsf check on development

--- app/controllers/osu_auth/sessions_controller.rb
+++ app/controllers/osu_auth/sessions_controller.rb
@@ -1,6 +1,8 @@
 module OsuAuth
   class SessionsController < ApplicationController
 
+    skip_before_action :verify_authenticity_token unless Rails.env.production?
+
     def create
       @user = User.omniauth(auth_hash: auth_hash)
       self.current_user = @user
