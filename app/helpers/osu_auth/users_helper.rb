bf2f77a6d3dc8c101701223a3387fb73ad303723
Brian Kildow <bkildow@gmail.com>
2015-01-30 10:34:24 -0500

    Refactored links to helpers and added permissions

--- app/helpers/osu_auth/users_helper.rb
+++ app/helpers/osu_auth/users_helper.rb
@@ -1,4 +1,24 @@
 module OsuAuth
   module UsersHelper
+
+    def create_user_link
+      if policy(@users).create?
+        link_to new_user_path, class: 'btn btn-primary', role: 'button' do
+          raw('<span class="glyphicon glyphicon-plus"></span> New User')
+        end
+      end
+    end
+
+    def edit_user_link(user)
+      if policy(user).update?
+        link_to 'Edit', edit_user_path(user)
+      end
+    end
+
+    def delete_user_link(user)
+      if policy(user).destroy?
+        link_to 'Delete', user_path(user), method: :delete, data: {confirm: 'Are you sure?'}
+      end
+    end
   end
 end
