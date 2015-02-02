be95d608b3a505fbf4d578bc14e1d147ee94bf72
Brian Kildow <bkildow@gmail.com>
2015-02-02 15:56:18 -0500

    Added not authorized message

--- lib/osu_auth/controller_helpers.rb
+++ lib/osu_auth/controller_helpers.rb
@@ -7,6 +7,9 @@ module OsuAuth
       helper_method :current_user
       attr_writer :current_user
       include Pundit
+
+      # Show an error page when action is not authorized with pundit
+      rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
     end
 
     private
@@ -22,6 +25,11 @@ module OsuAuth
     def current_user
       @current_user ||= GlobalID::Locator.locate session[:current_user] if session[:current_user]
     end
+
+    def user_not_authorized
+      render status: :forbidden, text: 'You are not authorized to perform this action.'
+    end
+
   end
 
 end
