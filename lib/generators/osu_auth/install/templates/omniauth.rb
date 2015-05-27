b3879bdf6e18d4d1463eae7bc6adc9cab81123e9
Brian Kildow <bkildow@gmail.com>
2015-05-27 08:44:29 -0400

    Fixed emplid mapping

--- lib/generators/osu_auth/install/templates/omniauth.rb
+++ lib/generators/osu_auth/install/templates/omniauth.rb
@@ -8,12 +8,12 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer, dev_options unless Rails.env.production?
 
   shib_options = {
-      uid_field: 'osuid',
+      uid_field: 'employeeNumber',
       info_fields: {
           name_n: lambda { |request_param| request_param.call('eppn').chomp('@osu.edu') },
           first_name: 'givenName',
           last_name: 'sn',
-          emplid: 'osuid'
+          emplid: 'employeeNumber'
       },
       debug: false
   }
--- lib/osu_auth/version.rb
+++ lib/osu_auth/version.rb
@@ -1,3 +1,3 @@
 module OsuAuth
-  VERSION = '1.1.3'
+  VERSION = '1.1.4'
 end
