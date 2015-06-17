5c22bd98b32903a2e2cacc658770dbe9ba1b647f
Brian Kildow <bkildow@gmail.com>
2015-06-17 10:47:04 -0400

    Changed redirect path after masquerading

--- app/controllers/osu_auth/users_controller.rb
+++ app/controllers/osu_auth/users_controller.rb
@@ -64,7 +64,7 @@ module OsuAuth
     def masquerade
       session[:current_user] = @user.to_gid.to_s
       flash[:notice] = "You are now logged in as #{@user.display_name} (#{@user.name_n})"
-      redirect_to users_path
+      redirect_to root_url
     end
 
     private
