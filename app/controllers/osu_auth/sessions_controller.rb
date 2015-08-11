1a25f94fdaaacbaae0474a26741ad8960ff829a3
Brian Kildow <bkildow@gmail.com>
2015-08-11 08:54:27 -0400

    Added logging for omniauth request hash

--- app/controllers/osu_auth/sessions_controller.rb
+++ app/controllers/osu_auth/sessions_controller.rb
@@ -5,6 +5,7 @@ module OsuAuth
     skip_before_action :authenticate_user!
 
     def create
+      logger.debug "OSU_AUTH -- : Attempting to login with omniauth request hash: #{request.env['omniauth.auth']}"
       user = User.omniauth(auth_hash)
       session[:current_user] = user.to_gid.to_s
       redirect_to session.delete(:osu_auth_redirect) || '/'
